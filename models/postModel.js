e5a124f18dc8d9e86cebce0d3de7ab0f185d0aa9
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-13 10:53:09 +0800

    feat: add postModel.js

---
+++ models/postModel.js
@@ -0,0 +1,33 @@
+const pool = require("../db/pool");
+
+const create = async (userId, title, message) => {
+  const result = await pool.query(
+    "INSERT INTO posts (user_id, title, message) VALUES ($1, $2, $3) RETURNING *",
+    [userId, title, message],
+  );
+  return result.rows[0];
+};
+
+const getById = async (id) => {
+  const result = await pool.query("SELECT * FROM posts WHERE id=$1", [id]);
+  return result.rows[0];
+};
+
+const getAll = async () => {
+  const result = await pool.query("SELECT * FROM posts");
+  return result.rows;
+};
+
+const remove = async (id) => {
+  const result = await pool.query("DELETE FROM posts WHERE id=$1 RETURNING *", [
+    id,
+  ]);
+  return result.rows[0];
+};
+
+module.exports = {
+  create,
+  getById,
+  getAll,
+  remove,
+};
