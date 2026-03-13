41ec88586a7d57b2f5cdbd3f55ec7b25aee5176d
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-13 09:37:29 +0800

    chore: set up app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+require("dotenv").config;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.get("/", (req, res) => res.render("index"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}!`);
+});
