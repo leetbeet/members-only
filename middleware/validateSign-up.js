153f4ba61b5cb32c45e7c14dc3d554ee68013706
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-16 11:11:07 +0800

    feat: add validation for sign-up

---
+++ middleware/validateSign-up.js
@@ -0,0 +1,31 @@
+const { body } = require("express-validator");
+
+exports.validatePost = [
+  body("firstName")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("First name required")
+    .isLength({ max: 20 })
+    .withMessage("First name must be at most 20 characters long"),
+  body("lastName")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Last name required")
+    .isLength({ max: 20 })
+    .withMessage("Last name must be at most 20 characters long"),
+  body("username")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Username required")
+    .isLength({ max: 15 })
+    .withMessage("Username must be at most 15 characters long"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 12 })
+    .withMessage("Password must be atleast 12 characters long"),
+];
