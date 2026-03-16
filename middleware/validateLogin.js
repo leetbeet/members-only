f405e465f65858fc93f5d56b0ba165ec98172299
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-16 11:07:54 +0800

    feat: add validation for login

---
+++ middleware/validateLogin.js
@@ -0,0 +1,12 @@
+const { body } = require("express-validator");
+
+exports.validatePost = [
+  body("username")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Username required")
+    .isLength({ max: 15 })
+    .withMessage("Username must be at most 15 characters long"),
+  body("password").trim().notEmpty().withMessage("Password requried"),
+];
