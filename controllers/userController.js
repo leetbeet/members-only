d7b6de0937c1e4c3c3dc155bcbc3408d72caacb9
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-17 11:00:31 +0800

    refactor: add functions to show login and sign-up page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,21 @@
 const userModel = require("../models/userModel");
 
+const showCreate = async (req, res) => {
+  res.render("sign-up");
+};
+
 const create = async (req, res) => {
   const { firstName, lastName, password } = req.body;
   await userModel.create(firstName, lastName, password);
   res.redirect("/");
 };
 
+const showLogin = async (req, res) => {
+  res.render("login");
+};
+
 module.exports = {
+  showCreate,
   create,
+  showLogin,
 };
