6770f4f1342e5095bbc4c36e7ca7bc5f7e15b7cf
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-17 10:47:21 +0800

    fix: method call

--- middleware/passport.js
+++ middleware/passport.js
@@ -28,7 +28,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await getById(id);
+    const user = await userModel.getById(id);
 
     done(null, user);
   } catch (err) {
