ea9bed6dddc8dd64685e80ac41ea65bd770a9216
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-17 11:43:14 +0800

    fix: import and passport.js usage

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const path = require("node:path");
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("dotenv").config();
+
 const app = express();
-require("dotenv").config;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-const assetsPath = path.join(__dirname, "public");
-app.use(express.static(assetsPath));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use((req, res, next) => {
   res.locals.user = req.user;
   next();
@@ -16,9 +29,6 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => res.render("index"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, (error) => {
-  if (error) {
-    throw error;
-  }
+app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
 });
