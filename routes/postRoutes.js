06e87a1f3573952575a9d2eafe508f003b4a4174
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-19 09:44:05 +0800

    feat: add auth to post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,17 +3,20 @@ const router = express.Router();
 const controller = require("../controllers/postController");
 const { validatePost } = require("../middleware/validatePost");
 const handleValidation = require("../middleware/handleValidation");
+const { ensureAuthenticated, ensureAdmin } = require("../middleware/auth");
 
 router.get("/", controller.list);
 
-router.get("/create", controller.showCreate);
+router.get("/create", ensureAuthenticated, controller.showCreate);
+
 router.post(
   "/create",
+  ensureAuthenticated,
   validatePost,
   handleValidation("post"),
   controller.create,
 );
 
-router.post("/:id/delete", controller.remove);
+router.post("/:id/delete", ensureAdmin, controller.remove);
 
 module.exports = router;
